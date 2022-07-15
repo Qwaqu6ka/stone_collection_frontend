7c839a7c441a8d3666cd4b289f4aa8000f104fb5
Qwaqu6ka <r.bortsov@yandex.ru>
2022-07-15 10:02:11 +0300

    пофиксил футер

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,12 +1,12 @@
 <template>
   <footer class="py-3 footer__down">
-    <p class="text-center text-muted">© 2022</p>
+    <p class="text-center">Институт минералов</p>
+    <p class="text-center text-muted">2022</p>
   </footer>
 </template>
 
 <style lang="scss">
 .footer__down {
-  position: absolute;
   width: 100%;
   bottom: 0;
   background-color: rgba(0, 0, 0, 0.05);
